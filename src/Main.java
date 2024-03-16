d87515b24ca5f30f926cde7753bc0094f5dc6afc
Cong Vu <116829489+CongLiv@users.noreply.github.com>
2024-03-16 14:39:13 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+        Thread thread1 = new Thread(() -> {
+            int sum = 0;
+            for (int number : numbers) {
+                sum += number;
+            }
+            System.out.println("Phần tử lớn nhất trong mảng là: " + sum);
+        });
 
+        Thread thread2 = new Thread(() -> {
+            int max = numbers[0];
+            for (int number : numbers) {
+                if (number > max) {
+                    max = number;
+                }
+            }
+            System.out.println("Tổng của các phần tử trong mảng là: " + max);
+        });
 
-        Viết mã của bạn vào đây
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     
     }
 }
